990042faedc675b54b7fb2c36f42eadd8cf79181
Rybchynskyi <alexandr.rybchinskij@gmail.com>
2023-02-16 16:09:00 +0200

    Make adminPanel more protected

--- models/login.php
+++ models/login.php
@@ -1,11 +1,20 @@
 <?php
+
+require_once '../config/connect.php';
+
 $user = $_POST['user'];
 $password = $_POST['password'];
 
-if ($user == "All4Ukraine" and $password == "qwerty") {
-    header('Location: ../bidlist.php');
+$systemUser = mysqli_query($connect,"SELECT value FROM `params` WHERE param='login'");
+$systemUser = mysqli_fetch_array($systemUser);
+
+$systemPass = mysqli_query($connect,"SELECT value FROM `params` WHERE param='pass'");
+$systemPass = mysqli_fetch_array($systemPass);
+
+if ($user == $systemUser['value'] and $password == $systemPass['value']) {
     setcookie("login", $user, time() + 14 * 86400, "/");
     setcookie("password", $password, time() + 14 * 86400, "/");
+    header('Location: ../bidlist.php');
 }
 else {
     header('Location: ../login.php');
